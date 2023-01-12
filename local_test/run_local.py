2c02d699aea84d4e1430cefd010584930ef2e014
Abu Desai <abhyuday.desai@gmail.com>
2023-01-11 22:44:09 -0800

    empty hyperparameters file; delete examples folder

--- local_test/run_local.py
+++ local_test/run_local.py
@@ -111,11 +111,6 @@ def copy_example_files(dataset_name):
         f"{local_datapath}/{dataset_name}/{dataset_name}_test.csv",
         os.path.join(test_data_path, f"{dataset_name}_test.csv"),
     )
-    # hyperparameters
-    shutil.copyfile(
-        "./examples/hyperparameters.json",
-        os.path.join(hyper_param_path, "hyperparameters.json"),
-    )
 
 
 def run_HPT(num_hpt_trials):
